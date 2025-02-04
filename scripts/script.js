6142cc0431fbb398555bb5f4dcf53167f19ecb3d
HasanAlshehabi <Hasan.alshehabi99@gmail.com>
2025-02-04 14:00:51 +0100

    Highscore displayed

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,7 @@ const gameFieldRef = document.getElementById('gameField');
 const audioRef = document.querySelector('audio');
 const highScoreRef = document.querySelector('#highScore');
 const highScoreListRef = document.querySelector('#highScoreList');
+const winMsgRef = document.getElementById('winMsg');
 
 let choosenGender;
 let totalPokemons = 10;
@@ -89,10 +90,10 @@ function catchPokemon(pokemon, pokemonImg, ball) {
 function endGame(){
   oGameData.endTimeInMilliseconds();
   const timeScore = oGameData.nmbrOfMilliseconds();
+  document.querySelectorAll(".pokemons").forEach(pokemon => pokemon.remove());
   highScoreRef.classList.remove('d-none');
-	gameFieldRef.classList.add('d-none');
-  updateHighScore(timeScore);
-  //display highscore
+  let highScoreArray = updateHighScore(timeScore);
+  displayHighScore(highScoreArray);
 }
 
 function updateHighScore(timeScore){
@@ -106,6 +107,16 @@ function updateHighScore(timeScore){
     highScoreArray.pop()
   }
   localStorage.setItem('highScoreArray', JSON.stringify(highScoreArray));
+  return highScoreArray;
+}
+
+function displayHighScore(highScoreArray) {
+    winMsgRef.textContent = `You are the best trainer ${oGameData.trainerName}!!!`
+    for(let i = 0; i < highScoreArray.length; i++){
+        const li = document.createElement("li");
+        li.textContent = `${i+1}: ${highScoreArray[i].name}: ${highScoreArray[i].time} ms`;
+        highScoreListRef.appendChild(li);
+    }
 }
 
 function validateGender() {
